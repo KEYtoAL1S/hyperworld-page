d9b801da91429a1901cfb22fe27d43726eed60b6
KEYtoAL1S <127687869+KEYtoAL1S@users.noreply.github.com>
2023-05-05 16:14:34 -0400

    Update get.js

--- get.js
+++ get.js
@@ -2,7 +2,7 @@ fetch(`https://games.roproxy.com/v1/games?universeIds=2332891023`) // get
   .then(response => response.json())
   .then(data => {
     const resultElement = document.getElementById("visits");
-    resultElement.innerHTML = `${data}`; // ??
+    resultElement.innerHTML = `${data.visits}` + ' Visits'; // ??
   })
   .catch(error => {
     console.error(error);
