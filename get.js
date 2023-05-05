6fd87536bf75ee61dab462f25fcd4d591926bc30
KEYtoAL1S <127687869+KEYtoAL1S@users.noreply.github.com>
2023-05-04 22:02:45 -0400

    Update get.js

--- get.js
+++ get.js
@@ -1,12 +1,8 @@
-const universeId = 2332891023;
-const namespace = "Percentile";
-const key = "Dataset";
-
-fetch(`https://data.roproxy.com/datastore/get-json?universeId=${universeId}&namespace=${namespace}&key=${key}`)
+fetch(`https://games.roblox.com/v1/games?universeIds=2332891023`) // get
   .then(response => response.json())
   .then(data => {
-    const resultElement = document.getElementById("result");
-    resultElement.innerHTML = `Datastore value: ${data}`;
+    const resultElement = document.getElementById("visits");
+    resultElement.innerHTML = `${data}`; // ??
   })
   .catch(error => {
     console.error(error);
