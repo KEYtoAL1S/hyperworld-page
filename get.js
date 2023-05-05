dc50d97ccbb4fce49f046b53cb981ce8a7c865f2
KEYtoAL1S <keytoal1s@gmail.com>
2023-05-05 19:13:40 -0400

    Service details

--- get.js
+++ get.js
@@ -4,12 +4,20 @@ fetch(`https://games.roproxy.com/v1/games?universeIds=2332891023`) // get game d
     const resultElement_visit = document.getElementById("visits");
     const resultElement_favorite = document.getElementById("fav");
     const resultElement_playing = document.getElementById("playing");
-    
+    const resultElement_status = document.getElementById("status");
     const gameData = tab.data[0]; // Assuming that we want to display data for the first game in the array
     
-    resultElement_visit.innerHTML = `${gameData.visits.toLocaleString()}` + ' Visits';
-    resultElement_favorite.innerHTML = `${gameData.favoritedCount.toLocaleString()}` + ' Favorited'; 
-    resultElement_playing.innerHTML = `${gameData.playing.toLocaleString()}` + ' Online';
+    resultElement_visit.innerHTML = `${gameData.visits.toLocaleString()}` + '';
+    resultElement_favorite.innerHTML = `${gameData.favoritedCount.toLocaleString()}` + ''; 
+    resultElement_playing.innerHTML = `${gameData.playing.toLocaleString()}` + '';
+
+    if (gameData.name.includes("[OUT]")) {
+      resultElement_status.innerHTML = 'OUT OF SERVICE'; 
+    } else {
+      resultElement_status.innerHTML = 'ONLINE';
+    }
+    
+
   })
   .catch(error => {
     console.error(error);
