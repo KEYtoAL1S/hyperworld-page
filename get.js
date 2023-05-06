60b36d4a8f0ae19ab8909118e8841b4b26c02e98
KEYtoAL1S <keytoal1s@gmail.com>
2023-05-06 10:06:08 -0400

    more changes need to work on main game im losing visits

--- get.js
+++ get.js
@@ -13,7 +13,6 @@ fetch(`https://games.roproxy.com/v1/games?universeIds=2332891023`) // get game d
 resultElement_favorite.innerHTML = "Loading...";
 resultElement_playing.innerHTML = "Loading...";
 resultElement_status.innerHTML = "Loading...";
-resultElement_visit.style.fontSize = "0.8rem";
 resultElement_visit.style.letterSpacing = "15px";
 
     
@@ -68,7 +67,6 @@ resultElement_visit.style.letterSpacing = "15px";
       resultElement_favorite.innerHTML = "Loading...";
       resultElement_playing.innerHTML = "Loading...";
       resultElement_status.innerHTML = "Loading...";
-      resultElement_visit.style.fontSize = "0.8rem";
       resultElement_visit.style.letterSpacing = "15px";
 
           const updatedGameData = tab.data[0];
@@ -85,7 +83,6 @@ resultElement_visit.style.letterSpacing = "15px";
           }
           
     // Revert the text and styles of the result elements to their initial values
-    resultElement_visit.style.fontSize = "45px";
     resultElement_visit.style.letterSpacing = "2px";
     resultElement_visit.innerHTML = `${gameData.visits.toLocaleString()}` + '';
     resultElement_favorite.innerHTML = `${gameData.favoritedCount.toLocaleString()}` + '';
