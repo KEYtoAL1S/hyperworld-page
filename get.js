9d1729ba8c2a68ae385f11eeb844e0402c42d144
KEYtoAL1S <keytoal1s@gmail.com>
2023-05-05 21:45:05 -0400

    site redesign tweening

--- get.js
+++ get.js
@@ -6,17 +6,92 @@ fetch(`https://games.roproxy.com/v1/games?universeIds=2332891023`) // get game d
     const resultElement_playing = document.getElementById("playing");
     const resultElement_status = document.getElementById("status");
     const gameData = tab.data[0]; // Assuming that we want to display data for the first game in the array
-    
-    resultElement_visit.innerHTML = `${gameData.visits.toLocaleString()}` + '';
-    resultElement_favorite.innerHTML = `${gameData.favoritedCount.toLocaleString()}` + ''; 
-    resultElement_playing.innerHTML = `${gameData.playing.toLocaleString()}` + '';
 
+    const tweentime = 2
+    
+    resultElement_visit.innerHTML = "Loading...";
+resultElement_favorite.innerHTML = "Loading...";
+resultElement_playing.innerHTML = "Loading...";
+resultElement_status.innerHTML = "Loading...";
+resultElement_visit.style.fontSize = "0.8rem";
+resultElement_visit.style.letterSpacing = "15px";
+
+    
+
+    // Create a tween for each number element
+    const visitTween = new TWEEN.Tween({ value: 0 })
+      .to({ value: gameData.visits }, tweentime*1e3)
+      .onUpdate(({ value }) => resultElement_visit.innerHTML = `${Math.floor(value).toLocaleString()}` + '');
+    
+    const favoriteTween = new TWEEN.Tween({ value: 0 })
+      .to({ value: gameData.favoritedCount }, tweentime*1e3)
+      .onUpdate(({ value }) => resultElement_favorite.innerHTML = `${Math.floor(value).toLocaleString()}` + '');
+    
+    const playingTween = new TWEEN.Tween({ value: 0 })
+      .to({ value: gameData.playing }, tweentime*1e3)
+      .onUpdate(({ value }) => resultElement_playing.innerHTML = `${Math.floor(value).toLocaleString()}` + '');
+    
+    // Start the tweens
+    visitTween.start();
+    favoriteTween.start();
+    playingTween.start();
+    
+    // Update the tweens every frame
+    function animate() {
+      requestAnimationFrame(animate);
+      TWEEN.update();
+    }
+    animate();
+    
     if (gameData.name.includes("[OUT]")) {
       resultElement_status.innerHTML = 'OUT OF SERVICE'; 
     } else {
       resultElement_status.innerHTML = 'ONLINE';
     }
+    // Revert the text and styles of the result elements to their initial values
+    resultElement_visit.style.fontSize = "45px";
+    resultElement_visit.style.letterSpacing = "-2px";
+    resultElement_visit.innerHTML = `${gameData.visits.toLocaleString()}` + '';
+    resultElement_favorite.innerHTML = `${gameData.favoritedCount.toLocaleString()}` + '';
+    resultElement_playing.innerHTML = `${gameData.playing.toLocaleString()}` + '';
+    resultElement_status.innerHTML = resultElement_status.innerHTML === 'ONLINE' ? 'ONLINE' : 'OUT OF SERVICE';
+
+    // Update data every 60 seconds
+    setInterval(() => {
+      fetch(`https://games.roproxy.com/v1/games?universeIds=2332891023`)
+        .then(response => response.json())
+        .then(tab => {
+                // Set the text and style of the result elements to indicate loading
+      resultElement_visit.innerHTML = "Loading...";
+      resultElement_favorite.innerHTML = "Loading...";
+      resultElement_playing.innerHTML = "Loading...";
+      resultElement_status.innerHTML = "Loading...";
+      resultElement_visit.style.fontSize = "0.8rem";
+      resultElement_visit.style.letterSpacing = "2px";
+
+          const updatedGameData = tab.data[0];
+          visitTween.to({ value: updatedGameData.visits }, 1000).start();
+          favoriteTween.to({ value: updatedGameData.favoritedCount }, 1000).start();
+          playingTween.to({ value: updatedGameData.playing }, 1000).start();
     
+          if (updatedGameData.name.includes("[OUT]")) {
+            resultElement_status.innerHTML = 'OUT OF SERVICE'; 
+          } else {
+            resultElement_status.innerHTML = 'ONLINE';
+          }
+          
+    // Revert the text and styles of the result elements to their initial values
+    resultElement_visit.style.fontSize = "45px";
+    resultElement_visit.style.letterSpacing = "-2px";
+    resultElement_visit.innerHTML = `${gameData.visits.toLocaleString()}` + '';
+    resultElement_favorite.innerHTML = `${gameData.favoritedCount.toLocaleString()}` + '';
+    resultElement_playing.innerHTML = `${gameData.playing.toLocaleString()}` + '';
+    resultElement_status.innerHTML = resultElement_status.innerHTML === 'ONLINE' ? 'ONLINE' : 'OUT OF SERVICE';
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }, 60000);
 
   })
   .catch(error => {
