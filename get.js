77660dd2e382c8322af258db4f1adaeb9866d346
KEYtoAL1S <keytoal1s@gmail.com>
2023-05-08 21:48:07 -0400

    new stuff prevent from tween from 0

--- get.js
+++ get.js
@@ -6,22 +6,23 @@ fetch(`https://games.roproxy.com/v1/games?universeIds=2332891023`) // get game d
     const resultElement_playing = document.getElementById("playing");
     const resultElement_like = document.getElementById("like");
     const resultElement_status = document.getElementById("status");
-
-    let visit_prev = 0
-    let fav_prev = 0
-    let playing_prev = 0
-
-    const gameData = tab.data[0]; // Assuming that we want to display data for the first game in the array
-
+    const gameData = tab.data[0];
     const tweentime = 0.5
-    
-    resultElement_visit.innerHTML = "Loading...";
-resultElement_favorite.innerHTML = "Loading...";
-resultElement_playing.innerHTML = "Loading...";
-resultElement_status.innerHTML = "Loading...";
 
+    let visit_prev = gameData.visits;
+    let fav_prev = gameData.favoritedCount;
+    let playing_prev = gameData.playing;
+    let past_visit_prev = gameData.visits;
+    let past_fav_prev = gameData.favoritedCount;
+    let past_playing_prev = gameData.playing;
+    let repetition = 0
     
-
+    resultElement_visit.innerHTML = "Fetching...";
+    resultElement_favorite.innerHTML = "Fetching...";
+    resultElement_playing.innerHTML = "Fetching...";
+    resultElement_status.innerHTML = 'PARSING';
+    resultElement_status.style.background = '#00000073'
+    
     // Create a tween for each number element
     const visitTween = new TWEEN.Tween({ value: visit_prev })
       .to({ value: gameData.visits }, tweentime*1e3)
@@ -46,7 +47,6 @@ resultElement_status.innerHTML = "Loading...";
       TWEEN.update();
     }
     animate();
-    
 
     if (gameData.name.includes("[MAINTENANCE]")) {
       resultElement_status.innerHTML = 'IN MAINTENANCE'; 
@@ -64,43 +64,41 @@ resultElement_status.innerHTML = "Loading...";
     // Update data every 60 seconds
     setInterval(() => {
       fetch(`https://games.roproxy.com/v1/games?universeIds=2332891023`)
-        .then(response => response.json())
+      .then(response => response.json())
         .then(tab => {
-                // Set the text and style of the result elements to indicate loading
-      resultElement_visit.innerHTML = "Loading...";
-      resultElement_favorite.innerHTML = "Loading...";
-      resultElement_playing.innerHTML = "Loading...";
-      resultElement_status.innerHTML = "Loading...";
+      resultElement_visit.innerHTML = "Fetching...";
+      resultElement_favorite.innerHTML = "Fetching...";
+      resultElement_playing.innerHTML = "Fetching...";
+      resultElement_status.innerHTML = 'PARSING';
+      resultElement_status.style.background = '#00000073'
+      const updatedGameData = tab.data[0];
+      visit_prev = updatedGameData.visits
+      fav_prev = updatedGameData.favoritedCount
+      playing_prev = updatedGameData.playing
+      visitTween.to({ value: updatedGameData.visits }, tweentime*1e3).start();
+      favoriteTween.to({ value: updatedGameData.favoritedCount }, tweentime*1e3).start();
+      playingTween.to({ value: updatedGameData.playing }, tweentime*1e3).start();
 
-          visit_prev = gameData.visits
-          fav_prev = gameData.favoritedCount
-          playing_prev = gameData.playing
-
-          const updatedGameData = tab.data[0];
-          visitTween.to({ value: updatedGameData.visits }, 1000).start();
-          favoriteTween.to({ value: updatedGameData.favoritedCount }, 1000).start();
-          playingTween.to({ value: updatedGameData.playing }, 1000).start();
-    
-          if (updatedGameData.name.includes("[MAINTENANCE]")) {
-            resultElement_status.innerHTML = 'IN MAINTENANCE'; 
-            resultElement_status.style.background = '#ff000073'
-          } else {
-            resultElement_status.innerHTML = 'ONLINE';
-            resultElement_status.style.background = '#00ff0073'
-          }
-          
+      if (updatedGameData.name.includes("[MAINTENANCE]")) {
+        resultElement_status.innerHTML = 'IN MAINTENANCE'; 
+        resultElement_status.style.background = '#ff000073'
+      } else {
+        resultElement_status.innerHTML = 'ONLINE';
+        resultElement_status.style.background = '#00ff0073'
+      }
+      
     // Revert the text and styles of the result elements to their initial values
     resultElement_visit.innerHTML = `${gameData.visits.toLocaleString()}` + '';
     resultElement_favorite.innerHTML = `${gameData.favoritedCount.toLocaleString()}` + '';
     resultElement_playing.innerHTML = `${gameData.playing.toLocaleString()}` + '';
     resultElement_status.innerHTML = resultElement_status.innerHTML === 'ONLINE' ? 'ONLINE' : 'IN MAINTENANCE';
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }, 60000);
-
   })
   .catch(error => {
     console.error(error);
   });
+}, 10*1e3);
+
+})
+.catch(error => {
+  console.error(error);
+});
